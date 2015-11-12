b2520352af90ed58ed4196082d06b742d6dfcb53
Steffen <steffen@s18036174.onlinehome-server.info>
2015-11-12 14:40:36 +0100

    Initial Commit with Yeoman generated Angular.JS project

---
+++ .tmp/concat/scripts/scripts.js
@@ -0,0 +1,87 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name datacityApp
+ * @description
+ * # datacityApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('datacityApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name datacityApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the datacityApp
+ */
+angular.module('datacityApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name datacityApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the datacityApp
+ */
+angular.module('datacityApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+angular.module('datacityApp').run(['$templateCache', function($templateCache) {
+  'use strict';
+
+  $templateCache.put('views/about.html',
+    "<p>This is the about view.</p>"
+  );
+
+
+  $templateCache.put('views/main.html',
+    "<div class=\"jumbotron\"> <h1>'Allo, 'Allo!</h1> <p class=\"lead\"> <img src=\"images/yeoman.png\" alt=\"I'm Yeoman\"><br> Always a pleasure scaffolding your apps. </p> <p><a class=\"btn btn-lg btn-success\" ng-href=\"#/\">Splendid!<span class=\"glyphicon glyphicon-ok\"></span></a></p> </div> <div class=\"row marketing\"> <h4>HTML5 Boilerplate</h4> <p> HTML5 Boilerplate is a professional front-end template for building fast, robust, and adaptable web apps or sites. </p> <h4>Angular</h4> <p> AngularJS is a toolset for building the framework most suited to your application development. </p> <h4>Karma</h4> <p>Spectacular Test Runner for JavaScript.</p> </div>"
+  );
+
+}]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name datacityApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the datacityApp
+ */
+angular.module('datacityApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
