b9aa8cb1fe373c2b2b040acca6e67d4e93b91d3d
Nullmann <nullmann75@gmx.de>
2015-11-17 12:13:19 +0100

    DataCity hochgeladen
    Die  bereits erstellte Seite in das Yeoman-Projekt eingebunden

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,6 +7,8 @@
  * # MainCtrl
  * Controller of the datacityApp
  */
+ 
+ //Von Yeoman automatisch erstellte Funktions 
 angular.module('datacityApp')
   .controller('MainCtrl', function () {
     this.awesomeThings = [
@@ -15,3 +17,38 @@ angular.module('datacityApp')
       'Karma'
     ];
   });
+  
+function uploadWechseln(){
+	var uploadAusgewaehlt;
+	var ausgabe = "";
+	var upload = document.getElementById("uploadAusgabe");
+	
+	if (document.getElementById("uploadRadio").checked === true) {
+		ausgabe = document.getElementById("upload").innerHTML;
+	} else {
+		ausgabe = document.getElementById("datensatzAnzeigen").innerHTML;
+	}
+	
+	upload.innerHTML = "<div class='container'>" + ausgabe + "</div>";
+	document.getElementById("metadatenAuswahl").style="display: none;"
+}
+
+function ausgabe(){	
+	var ausgabe = document.getElementById("ausgabe");
+	ausgabe.innerHTML = 
+	"Fläche: " + document.getElementById("flaecheselektieren").value +
+	"<br>Höhe: " + document.getElementById("hoeheselektieren").value +
+	"<br>Farbe: " + document.getElementById("farbeselektieren").value;
+	
+	if (document.getElementById("farbenRadio1").checked === true){
+		ausgabe.innerHTML += "<br>Aufsteigend wurde selektiert";
+	} else {
+		ausgabe.innerHTML += "<br>Absteigend wurde selektiert";
+	}
+	
+	ausgabe.innerHTML +=
+	"<br>Grupperigung nach: " + document.getElementById("gruppierung").value +
+	"<br>Verbindungen bilden mit: " + document.getElementById("verbindung").value + 
+	"<br>Der Winkel: " + document.getElementById("winkelInput").value + "°" + 
+	"<br>Die Granularität: " + document.getElementById("granularInput").value + "%";
+}
