d409ed0b970701860cea5c381e1515dc8f551c37
Nullmann <nullmann75@gmx.de>
2015-12-02 17:54:10 +0100

    Externe Dateien importieren, distribution

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -243,25 +243,4 @@ App.controller('MainCtrl', function($scope, $http, $rootScope) {
 		},
 	};
 	$scope.data = exampleData;
-	
 });
-
-function ausgabe(){	
-	var ausgabe = document.getElementById("ausgabe");
-	ausgabe.innerHTML = 
-	"Fläche: " + document.getElementById("flaecheselektieren").value +
-	"<br>Höhe: " + document.getElementById("hoeheselektieren").value +
-	"<br>Farbe: " + document.getElementById("farbeselektieren").value;
-	
-	if (document.getElementById("farbenRadio1").checked === true){
-		ausgabe.innerHTML += "<br>Aufsteigend wurde selektiert";
-	} else {
-		ausgabe.innerHTML += "<br>Absteigend wurde selektiert";
-	}
-	
-	ausgabe.innerHTML +=
-	"<br>Grupperigung nach: " + document.getElementById("gruppierung").value +
-	"<br>Verbindungen bilden mit: " + document.getElementById("verbindung").value + 
-	"<br>Der Winkel: " + document.getElementById("winkelInput").value + "°" + 
-	"<br>Die Granularität: " + document.getElementById("granularInput").value + "%";
-}
