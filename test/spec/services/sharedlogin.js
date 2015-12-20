1719a21b7f6bc62a02c37e5b1680cb3125d17a71
Steffen Pegenau <steffen.pegenau@gmail.com>
2015-12-20 12:17:13 +0100

    Tests für sharedLogin-Service geschrieben

--- test/spec/services/sharedlogin.js
+++ test/spec/services/sharedlogin.js
@@ -11,8 +11,29 @@ describe('Service: sharedLogin', function () {
     sharedLogin = _sharedLogin_;
   }));
 
-  it('should do something', function () {
+  it('should recognize this service', function () {
     expect(!!sharedLogin).toBe(true);
   });
-
+  
+  it('should change the loggedIn status', function() {
+    sharedLogin.loggedIn = false;
+    expect(sharedLogin.getLoggedIn()).toBe(false);
+    sharedLogin.login("a", "a");
+    expect(sharedLogin.getLoggedIn()).toBe(true);
+    
+    // Test logout function
+    sharedLogin.logout();
+    expect(sharedLogin.getLoggedIn()).toBe(false);
+    expect(sharedLogin.loggedIn).toBe(false);
+    expect(sharedLogin.getUsername()).toBe("");
+    expect(sharedLogin.getPassword()).toBe("");
+  });
+  
+  it('should get the right username and password after login', function() {
+    sharedLogin.login("a", "a");
+    
+    expect(sharedLogin.getUsername()).toBe("a");
+    expect(sharedLogin.getPassword()).toBe("a");
+    
+  });
 });
