af70e09783c87386452551d05f65b0f9dfcb3bfa
Nullmann <nullmann75@gmx.de>
2016-01-07 20:45:25 +0100

    Versuch Ansichten zu löschen

--- app/scripts/services/rest.js
+++ app/scripts/services/rest.js
@@ -144,9 +144,9 @@ angular.module('datacityApp')
          * Löscht eine Collection
          * 
          * TODO Ansichten löschen
-         * TODO Meta-Daten löschen
          */
         this.deleteCollection = function(db, collection, fn) {
+            
             this.getCurrentETag(db, collection, function(etag) {
                 rest.deleteData(function(r) {
                     $log.info('Gelöscht:\t' + db + '/' + collection);
@@ -155,6 +155,22 @@ angular.module('datacityApp')
                     }
                 }, etag, db, collection, null);
             });
+             
+            /*      
+            this.getViewsOfCollection(collection, function (views) {
+                
+                for (var iteration in views) {
+                    console.log("Es wird gelöscht: ");
+                    console.log(views[iteration]);
+                    
+                    
+                    this.deleteView(views[iteration], function (response) {
+                        console.log("löschen funktioniert: ");
+                        console.log(response);
+                    });
+                }
+            });
+            */
         };
         
         /**
