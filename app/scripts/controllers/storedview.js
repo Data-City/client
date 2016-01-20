ac5e2c155fd70a7d126dd9ab6842f1a593bf6b19
Nullmann <nullmann75@gmx.de>
2016-01-20 16:21:07 +0100

    storedView wird richtig angezeigt!

--- app/scripts/controllers/storedview.js
+++ app/scripts/controllers/storedview.js
@@ -10,19 +10,36 @@
 
 angular.module('datacityApp')
   .controller('StoredviewCtrl', function ($scope, $route, $routeParams, $log, $http, $rootScope, sharedLogin, AGGR, REST) {
-      
-    var database = "prelife";
     
+    REST.setUsername(sharedLogin.getUsername());
+    REST.setPassword(sharedLogin.getPassword());
+
+    var databaseForCollections = "prelife";
+    var databaseForViews = "einstellungen";
+    var ansichten = "ansichten";
+    var baseurl = "https://pegenau.com:16392";
+
+    var WEBGL_DIV = 'Stadt';
     
     $scope.test = function() {
         console.log("window.location: ");
         console.log(window.location);
-    }
+    };
     
-     var storedJSON = JSON.parse(window.locations.href.split("?webGLSettings=")[1]);
+     var url = window.location.href;
+     
+     url = url.replace(/%7B/g, '{');
+     url = url.replace(/%7D/g, '}');
+     url = url.replace(/%22/g, '"');
+     url = url.replace(/%5B/g, '[');
+     url = url.replace(/%5D/g, ']');
+     
+     var storedJSON = JSON.parse(url.split("?webGLSettings=")[1]);
+     
+     console.log(storedJSON);
 
     // DIE COLLECTION WIRD GEHOLT (DU MUSST DIE COLLID SETZEN)
-    REST.getDocuments(database, storedJSON.collID, function (collection) { //Hier musst du die CollectionID richtig setzen
+    REST.getDocuments(databaseForCollections, storedJSON.collID, function (collection) {
         $scope.chosenCollection = collection;
         $scope.collections = null;
         
@@ -34,7 +51,18 @@ angular.module('datacityApp')
             
             // CODE HIER DRIN 
             
-        }, database, collection, storedJSON._id); //HIER musst du die view richtig setzen
+            console.log("Ausgewählte Collection: ");
+            console.log($scope.chosenCollection);
+            
+            console.log("Ausgewählte Ansicht:");
+            console.log($scope.chosenView);
+            
+            var settings = storedJSON;
+            
+           // drawCity(collection.data._embedded['rh:doc'], $scope.chosenView, WEBGL_DIV, settings);
+           drawCity(collection.data._embedded['rh:doc'], $scope.chosenView, WEBGL_DIV, settings);
+            
+        }, databaseForViews, ansichten, storedJSON._id);
         
     });
     
