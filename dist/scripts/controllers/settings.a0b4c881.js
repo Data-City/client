1e193336bd8a23779c58874af943cd47234edc05
Steffen Pegenau <steffen.pegenau@gmail.com>
2016-01-26 11:30:44 +0100

    Neue Dist mit Settings und Icon

---
+++ dist/scripts/controllers/settings.a0b4c881.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name datacityApp.controller:SettingsCtrl
+ * @description
+ * # SettingsCtrl
+ * Controller of the datacityApp
+ */
+angular.module('datacityApp')
+    .constant('SETTINGS', {
+        databaseForCollections: "prelife",
+        databaseForViews: "einstellungen",
+        collection: "ansichten",
+        baseurl: "https://pegenau.com:16392",
+
+        // Max. 16MB bei BSON in Aggregation 
+        max_docs_per_aggregation: 100000,
+
+        // Ab hier sollten eigentlich keine Änderungen nötig sein
+        WEBGL_DIV: 'Stadt',
+        meta_data_part: '_dc_',
+        meta_data_suffix: 'maxminavg',
+        aggregation_suffix: 'aggregation',
+        data_suffix: 'data',
+    });
+
+/*
+var databaseForCollections = "prelife";
+var databaseForViews = "einstellungen";
+var collection = "ansichten";
+var baseurl = "https://pegenau.com:16392";
+
+var WEBGL_DIV = 'Stadt';
+
+var dbWithCollections = "prelife";
+*/
