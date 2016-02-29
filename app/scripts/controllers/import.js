f986932d08c40eb31898d3d4d4d7f110bf4f656b
Steffen Pegenau <steffen.pegenau@gmail.com>
2016-02-29 19:12:41 +0100

    CSV-Datei wird interpretiert und hochgeladen, aber...
    ..noch nicht wirklich importiert. Die Meta-Daten fehlen.

--- app/scripts/controllers/import.js
+++ app/scripts/controllers/import.js
@@ -9,15 +9,48 @@
  */
 angular.module('datacityApp')
     .controller('ImportCtrl', function ($scope, $routeParams, sharedLogin, $log, REST, SETTINGS) {
-    /**    var db = SETTINGS.databaseForCollections;
+        /**    var db = SETTINGS.databaseForCollections;
+            REST.setUsername(sharedLogin.getUsername());
+            REST.setPassword(sharedLogin.getPassword());
+            $scope.collID = null;
+            
+            if ($routeParams.collID) {
+                $scope.collID = $routeParams.collID;
+            }*/
+        // var etag = REST.getCurrentETag(db, $scope.collID, function(result){});
+        // var result = REST.putOnCollection(db, $scope.collID, etag, $routeParams, function(result){});
+        
+        var uploadedObj = null;
+        var filename = null;
+        var db = SETTINGS.databaseForCollections;
         REST.setUsername(sharedLogin.getUsername());
         REST.setPassword(sharedLogin.getPassword());
-        $scope.collID = null;
         
-        if ($routeParams.collID) {
-            $scope.collID = $routeParams.collID;
-        }*/
-       // var etag = REST.getCurrentETag(db, $scope.collID, function(result){});
-       // var result = REST.putOnCollection(db, $scope.collID, etag, $routeParams, function(result){});
+        $scope.loader = false;
+
+        $scope.startImport = function () {
+            var fileInput = document.getElementById('csv-file');
+            filename = getFilename(fileInput);
+            $log.info(filename);
+            Papa.parse(fileInput.files[0], {
+                header: true,
+                dynamicTyping: true,
+                complete: function (obj) {
+                    uploadedObj = obj;
+                    startUpload();
+                }
+            });
+        };
         
+        var startUpload = function() {
+            REST.putOnCollection(db, filename, null, uploadedObj, function(result) {
+                $log.info(result);
+            });
+        };
+
+        var getFilename = function (fileInput) {
+            var path = fileInput.value;
+            var filenameWithExtension = path.split(/(\\|\/)/g).pop();
+            return filenameWithExtension.substr(0, filenameWithExtension.lastIndexOf('.')) || filenameWithExtension;
+        };
     });
