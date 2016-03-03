9bf5959d7f16851122aed14eae05869240ec65bf
Steffen Pegenau <steffen.pegenau@gmail.com>
2016-03-03 11:00:05 +0100

    Importeinstellungen werden jetzt auf Validität geprüft

--- app/scripts/controllers/import.js
+++ app/scripts/controllers/import.js
@@ -8,7 +8,7 @@
  * Controller of the datacityApp
  */
 angular.module('datacityApp')
-    .controller('ImportCtrl', function($scope, $routeParams, sharedLogin, $log, REST, SETTINGS) {
+    .controller('ImportCtrl', function ($scope, $routeParams, sharedLogin, $log, REST, SETTINGS) {
 
         var uploadedObj = null;
         var filename = null;
@@ -19,13 +19,19 @@ angular.module('datacityApp')
         $scope.msg = "Eine mögliche Nachricht!";
         $scope.percentage = 0;
         $scope.loader = false;
-        
-        $scope.leftCol="col-sm-6";
-        $scope.rightCol="col-sm-6";
-        $scope.useFilenameAsColName=true;
-        
 
-        $scope.startImport = function() {
+        $scope.leftCol = "col-sm-6";
+        $scope.rightCol = "col-sm-6";
+        $scope.useFilenameAsColName = true;
+        $scope.colName = "";
+        $scope.withConnections = false;
+
+
+        $scope.startImport = function () {
+            if (!settingsValid()) {
+                return false;
+            }
+
             $scope.msg = "Lade Daten in Browser-Cache...";
             $scope.percentage = 10;
             $scope.loader = true;
@@ -38,14 +44,14 @@ angular.module('datacityApp')
             Papa.parse(fileInput.files[0], {
                 header: true,
                 dynamicTyping: true,
-                complete: function(obj) {
+                complete: function (obj) {
                     uploadedObj = obj;
                     $scope.msg = "Lade Daten auf Server...";
                     $scope.percentage = 30;
-                    startUpload(function() {
+                    startUpload(function () {
                         $scope.msg = "Fertig (Eigentlich müsste hier noch eine Menge passieren...)";
                         $scope.percentage = 70;
-                        setTimeout(function() {
+                        setTimeout(function () {
                             $scope.loader = false;
                         }, 2000);
                     });
@@ -53,9 +59,49 @@ angular.module('datacityApp')
             });
         };
 
+        var settingsValid = function () {
+            var pathToCsvFile = document.getElementById('csv-file').value;
+            // Wurde Datei Datensätzen ausgewählt?
+            if (pathToCsvFile === "") {
+                alert("Bitte wählen Sie eine Datei mit Datensätzen aus!");
+                return false;
+            }
 
-        var startUpload = function(fn) {
-            REST.putOnCollection(db, filename, null, uploadedObj, function(result) {
+            // Handelt es sich um eine CSV-Datei?
+            if (getFileExtension(pathToCsvFile) !== "csv") {
+                alert("Bitte wählen Sie eine CSV-Datei aus!");
+                return false;
+            }
+            
+            // Wurde "eigener Name" gewählt aber keiner angegeben?
+            if (!$scope.useFilenameAsColName && $scope.colName === "") {
+                alert("Bitte geben Sie einen Namen ein oder nutzen Sie den Namen der CSV-Datei!");
+                return false;
+            }
+            
+            var connectionsFilePath = document.getElementById('connections-csv-file').value;
+            // Wurde Verbindungen nutzen gewählt, aber keine Datei gewählt?
+            if ($scope.withConnections && connectionsFilePath === "") {
+                alert("Bitte wählen Sie eine Datei mit Verbindungen aus oder deaktivieren Sie die Verbindungen!");
+                return false;
+            }
+            
+            // Ist die Verbindungsdatei keine CSV-Datei?
+            if (getFileExtension(connectionsFilePath) !== "csv") {
+                alert("Die Verbindungsdatei muss eine CSV-Datei sein!");
+                return false;
+            }
+            
+            // Jetzt müsste alles in Ordnung sein!
+            return true;
+        };
+
+        var getFileExtension = function (path) {
+            return path.substr(path.lastIndexOf('.') + 1);
+        };
+
+        var startUpload = function (fn) {
+            REST.putOnCollection(db, filename, null, uploadedObj, function (result) {
                 $log.info(result);
                 if (fn) {
                     fn();
@@ -63,7 +109,7 @@ angular.module('datacityApp')
             });
         };
 
-        var getFilename = function(fileInput) {
+        var getFilename = function (fileInput) {
             var path = fileInput.value;
             var filenameWithExtension = path.split(/(\\|\/)/g).pop();
             return filenameWithExtension.substr(0, filenameWithExtension.lastIndexOf('.')) || filenameWithExtension;
