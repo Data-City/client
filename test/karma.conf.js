08a4779e1a9965e341f371574094105d74fa9ff7
Steffen Pegenau <steffen.pegenau@gmail.com>
2016-03-04 10:41:50 +0100

    Pseudotest für loadingpanel raus. Closes #213

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -39,6 +39,7 @@ module.exports = function(config) {
       'bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.js',
       'bower_components/angular-bootstrap-slider/slider.js',
       'bower_components/papaparse/papaparse.js',
+      'bower_components/async/lib/async.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
