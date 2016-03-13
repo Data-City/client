5800cef57c9d24fac1a02644517cfa8d40ac5285
Nullmann <nullmann75@gmx.de>
2016-03-13 12:54:54 +0100

    dist
    - Optik (Navigationsleiste)
    - Abstände zwischen den Gebäuden dynamisch
    - Bedienung per Pfeiltasten verbessert
    - Highlichting der Gebäude mit Verbindungen
    - Speicherverbrauch verändert
    - Dimension "Name" wird gespeichert
    - etc

--- app/scripts/libs/webgl/dropDownMenue.js
+++ app/scripts/libs/webgl/dropDownMenue.js
@@ -35,7 +35,7 @@ var buildingInformation = {
     }*/
     "Verbindungen": false,
     "eingehendeVerbindungenaktivieren": false,
-    "ausgehendeVerbindungenaktivieren":false
+    "ausgehendeVerbindungenaktivieren": false
 };
 
 //entsteht, wenn Nutzer die Legende aendert
@@ -311,13 +311,13 @@ function setMenue(scene, aDistrict, camera, orbitControls, trackballControls, na
         buildingInformation["Verbindungen"] = value;
         highlightBuildingsWithConnections(value);
     })
-    h.add(buildingInformation, "eingehendeVerbindungenaktivieren").name("eingehende Verbindungen").onChange(function(value){
+    h.add(buildingInformation, "eingehendeVerbindungenaktivieren").name("eingehende Verbindungen").onChange(function(value) {
         buildingInformation["eingehendeVerbindungenaktivieren"] = value;
-        showIncomingConnections(value);      
+        showIncomingConnections(value);
     })
-    h.add(buildingInformation, "ausgehendeVerbindungenaktivieren").name("ausgehende Verbindungen").onChange(function(value){
-        buildingInformation["ausgehendeVerbindungenaktivieren"] = value;      
-    })
+    h.add(buildingInformation, "ausgehendeVerbindungenaktivieren").name("ausgehende Verbindungen").onChange(function(value) {
+            buildingInformation["ausgehendeVerbindungenaktivieren"] = value;
+        })
         /*h.add(buildingInformation, "remove").name("Ausblenden");
         h.add(buildingInformation, "undo").name("Ausblenden rückgängig");*/
 
@@ -479,27 +479,27 @@ function highlightBuildingsWithConnections(value) {
     var income = arr[0];
     var outgoing = arr[1];
     var hashmap = getBuildingsHashMap();
-     for(var y in hashmap){
+    for (var y in hashmap) {
         if (value) {
-                if (income[y] || outgoing[y]) {
-                    colorObject(hashmap[y], 0xFFBF00);
-                }
-            } else {
-                if (income[y] || outgoing[y]) {
-                    var hashMap = getBuildingsHashMap();
-                    var factor = getColorFactor(getExtrema(), hashMap[y]._color, "Color");
-                    colorObject(hashMap[y], (new THREE.Color(0xBDBDBD)).lerp(new THREE.Color(buildingColor), factor));
-                }
+            if (income[y] || outgoing[y]) {
+                colorObject(hashmap[y], 0xFFBF00);
             }
+        } else {
+            if (income[y] || outgoing[y]) {
+                var hashMap = getBuildingsHashMap();
+                var factor = getColorFactor(getExtrema(), hashMap[y]._color, "Color");
+                colorObject(hashMap[y], (new THREE.Color(0xBDBDBD)).lerp(new THREE.Color(buildingColor), factor));
+            }
+        }
     }
 }
 
-function showIncomingConnections(value){
+function showIncomingConnections(value) {
     var b = buildingInformation["name"];
     var arr = getCalls();
     var income = arr[0];
-    
-    
+
+
 }
 
 
