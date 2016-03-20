066614310b3296ae71a0899d3225494579494a54
Steffen Pegenau <steffen.pegenau@gmail.com>
2016-03-20 23:16:34 +0100

    Aufräumarbeiten. Closes #289

--- app/scripts/libs/webgl/ClientWebGL.js
+++ app/scripts/libs/webgl/ClientWebGL.js
@@ -46,36 +46,6 @@ function getControls() {
   return trackballControls;
 }
 
-
-
-function doDispose(obj) {
-  if (obj !== null) {
-    for (var i = 0; i < obj.children.length; i++) {
-      doDispose(obj.children[i]);
-    }
-    if (obj.geometry) {
-      obj.geometry.dispose();
-      obj.geometry = undefined;
-    }
-    if (obj.material) {
-      if (obj.material.materials) {
-        for (i = 0; i < obj.material.materials.length; i++) {
-          obj.material.materials[i].dispose();
-        }
-      } else {
-        obj.material.dispose();
-      }
-      obj.material = undefined;
-    }
-    if (obj.texture) {
-      obj.texture.dispose();
-      obj.texture = undefined;
-    }
-  }
-  obj = undefined;
-}
-
-
 /**
  * wird vom Client-Team aufgerufen und fuehrt alles aus, was getan werden muss, um die Stadtansicht zu erstellen
  * @param: data: JSON vom Datenbank-Team, das fuer jedes Gebaeude die Hoehe, Breite, Farbe, etc. gespeichert hat der Form
