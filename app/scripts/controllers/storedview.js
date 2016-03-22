4909bb81c18eb0fabb4dccc5105f655e1dfefa8a
Nullmann <nullmann75@gmx.de>
2016-03-22 15:34:37 +0100

    Alle Änderungen von view.js in storedview.js übernommen. #300

--- app/scripts/controllers/storedview.js
+++ app/scripts/controllers/storedview.js
@@ -54,11 +54,32 @@ angular.module('datacityApp')
         $scope.setLoaderSettings("Hole Daten...", 50);
         REST.getDocuments(databaseForCollections, storedJSON.collID + "_dc_data_" + storedJSON._id, function(collection) {
             REST.getData(function(viewResponse) {
-                $scope.chosenView = viewResponse.data;
-
-                if ($scope.chosenView.experimentalMode === false) {
-                    $scope.chosenView.typeOfConnections = 0;
+                var view = viewResponse.data;
+                
+                console.log(view);
+                
+                view.numberOfEntries = collection.data._returned;
+                if (view.numberOfEntries === 0) {
+                    window.alert("Die Filterung bzw. Aggregation wurde so eingestellt, dass keine Datensätze übrig bleiben!");
+                    $scope.setLoaderSettings("Der Ladevorgang wurde abgebrochen", 100);
+                    $scope.loader = false;
+                    return false;
                 }
+                view.dimensions.name = {
+                    name: view.dimensionSettings.name.name
+                };
+                view.dimensions.height = view.dimensionSettings.height.name;
+                view.dimensions.area = view.dimensionSettings.area.name;
+                view.dimensions.color = view.dimensionSettings.color.name;
+                view.buildingcolor = SETTINGS.farbefuerGebauede;
+
+                if (view.logScaling === undefined) {
+                    view.logScaling = {};
+                    view.logScaling.color = false;
+                    view.logScaling.width = false;
+                    view.logScaling.height = false;
+                }
+                
                 if (viewResponse.data.metaData.connectionsAvailable) {
                     $scope.setLoaderSettings("Rufe Verbindungsdaten ab...", 60);
                     REST.getDocuments(databaseForCollections, storedJSON.collID + "_dc_connections_incoming", function(incoming) {
@@ -68,13 +89,13 @@ angular.module('datacityApp')
                                 var outgoingConnections = outgoing.data._embedded['rh:doc'][0];
 
                                 $scope.setLoaderSettings("Beginne Aufbau der Stadt...", 65);
-                                drawCity(collection.data._embedded['rh:doc'], $scope.chosenView, WEBGL_DIV, settings, incomingConnections, outgoingConnections, $scope.setLoaderSettings);
+                                drawCity(collection.data._embedded['rh:doc'], view, WEBGL_DIV, settings, incomingConnections, outgoingConnections, $scope.setLoaderSettings);
                             }
                         });
                     });
                 } else {
                     $scope.setLoaderSettings("Beginne Aufbau der Stadt...", 65, $scope);
-                    drawCity(collection.data._embedded['rh:doc'], $scope.chosenView, WEBGL_DIV, settings, undefined, undefined, $scope.setLoaderSettings);
+                    drawCity(collection.data._embedded['rh:doc'], view, WEBGL_DIV, settings, undefined, undefined, $scope.setLoaderSettings);
                 }
             }, databaseForViews, ansichten, storedJSON._id);
         });
